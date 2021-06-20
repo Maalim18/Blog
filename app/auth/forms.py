36def6be3beacd495b11a50f3da7bbe29ba3ff60
Maalim18 <ali.mohamed@student.moringaschool.com>
2021-06-20 19:39:29 +0300

    add login form class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -23,8 +23,4 @@ class RegistrationForm(FlaskForm):
 
 
 
-class LoginForm(FlaskForm):
-    email = StringField('Your Email Address',validators=[Required(),Email()])
-    password = PasswordField('Password',validators =[Required()])
-    remember = BooleanField('Remember me')
-    submit = SubmitField('Sign In')        
+    
